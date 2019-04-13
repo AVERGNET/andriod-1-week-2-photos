4101a2cebf538b409ff673b442d11284bdc2cf67
Alexis Vergnet <a..v.vergnet@gmail.com>
2019-04-13 13:44:19 -0700

    f;alksfjd

--- app/src/main/java/com/ucsdextandroid1/photosapp/profile/ProfileViewHolder.java
+++ app/src/main/java/com/ucsdextandroid1/photosapp/profile/ProfileViewHolder.java
@@ -60,7 +60,8 @@ public class ProfileViewHolder extends RecyclerView.ViewHolder {
     public void bind(Profile profile) {
         currentProfile = profile;
 
-        Picasso.get().load(profile.getProfileImageUrl()).into(iconImage);
+        Picasso.get().load(profile.getProfileImageUrl()).placeholder(android.R.color.holo_red_light).into(iconImage);
+
         nameView.setText(profile.getFullName());
         postsCountView.setText(String.valueOf(profile.getPostCount()));
         followersCountView.setText(String.valueOf(profile.getFollowersCount()));
